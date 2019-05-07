25aaf41f08b51a289fd37c82af53544cffb742c0
ChunLing <17880188607@163.com>
2019-05-07 10:20:07 +0800

    第一次提交Maven-SSM

---
+++ jd-dao/src/main/java/com/oracle/mapper/MonsterMapper.java
@@ -0,0 +1,28 @@
+package com.oracle.mapper;
+
+import com.oracle.web.bean.Monster;
+import com.oracle.web.bean.SubMonster;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface MonsterMapper {
+   
+    int deleteByPrimaryKey(Integer monsterId);
+
+    int insert(Monster record);
+
+    Monster selectByPrimaryKey(Integer monsterId);
+
+    List<SubMonster> selectAll();
+
+    int updateByPrimaryKey(Monster record);
+    
+    List<SubMonster> selectAllWithOneSQL();
+
+	int selectCounts();
+
+	List<SubMonster> findByPage(int index);
+   
+}
